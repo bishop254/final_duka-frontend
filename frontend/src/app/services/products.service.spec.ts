2acc764f284949555b26df9b8d666b9ffebc048a
bishop254 <karanjachiuri@gmail.com>
2022-05-31 09:21:52 +0300

    Tested that our ProductsService works as expected.

--- frontend/src/app/services/products.service.spec.ts
+++ frontend/src/app/services/products.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpClientSpy: any; //Mocks the HttpClient module
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    //We mock the get function of the HttpClient module
+    httpClientSpy = {
+      get: jest.fn(),
+    };
+
+    service = new ProductsService(httpClientSpy); //Create a fake instance of the ProductsService
   });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  
+  it('should getProducts from API', () => {
+    const response = 'data';
+    let url = 'https://fakestoreapi.com/products';
+
+    jest.spyOn(httpClientSpy, 'get').mockReturnValue(of(response));
+    service.getProducts();
+    expect(httpClientSpy.get).toBeCalledTimes(1); //Test if the GET method is triggered
+    expect(httpClientSpy.get).toHaveBeenCalledWith(url); //Test if the GET request makes a call with our URL
   });
 });
