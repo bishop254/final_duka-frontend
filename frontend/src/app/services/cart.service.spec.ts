80c1279d5639ea91c0dfa0227cf2c412e1554f45
bishop254 <karanjachiuri@gmail.com>
2022-05-31 12:35:11 +0300

    Tested the CartService functionality ensuring it worked as expected.

--- frontend/src/app/services/cart.service.spec.ts
+++ frontend/src/app/services/cart.service.spec.ts
@@ -6,11 +6,45 @@ describe('CartService', () => {
   let service: CartService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CartService);
+    service = new CartService();
   });
 
-  it('should be created', () => {
+  it('should be created (Cart Service)', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call updateCartList and push the product to cartItems array', () => {
+    let product = [{ name: 'prod1' }];
+
+    service.updateCartList(product);
+
+    expect(service.cartItems).toEqual(product);
+
+    service.cartList$.subscribe((resp) => {
+      let items = resp;
+      expect(service.cartItems).toEqual(items);
+    });
+  });
+
+  it('should remove an item from the cart', () => {
+    let product = { name: 'two' };
+    service.cartItems = [{ name: 'one' }, { name: 'two' }, { name: 'three' }];
+
+    service.removeCartItem(product);
+
+    expect(service.cartItems).toHaveLength(2);
+
+    service.cartList$.subscribe((resp) => {
+      let items = resp;
+      expect(service.cartItems).toEqual(items);
+    });
+  });
+
+  it('should empty the cart', () => {
+    service.cartItems = [{ name: 'one' }, { name: 'two' }, { name: 'three' }];
+
+    service.emptyCart();
+
+    expect(service.cartItems).toHaveLength(0);
   });
 });
