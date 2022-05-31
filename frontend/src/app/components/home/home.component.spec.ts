630f0a0716cf4eb10cdd42f4148b51d2c3bcee59
bishop254 <karanjachiuri@gmail.com>
2022-05-31 09:28:35 +0300

    Performed tests to ensure our Home component worked as expected.

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,25 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: HomeComponent;
+  let prodServMock: any;
+  let cartServMock: any;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    prodServMock = {
+      getProducts: jest.fn(),
+    };
+
+    cartServMock = {
+      updateCart: jest.fn(),
+    };
+    fixture = new HomeComponent(prodServMock, cartServMock);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(fixture).toBeTruthy();
+    expect(fixture).toBeDefined();
+  });
+
+  it('should ensure products, allCategoryProduct and categoryNames to be empty at first', () => {
+    expect(fixture.products).toBeFalsy();
+    expect(fixture.allCategoryProducts).toBeFalsy();
+    expect(fixture.categoryNames).toBeFalsy();
+  });
+
+  it('should update products Array with API data', () => {
+    const response = [{}, {}, {}]; //Dummy API reponse data
+    jest.spyOn(prodServMock, 'getProducts').mockReturnValue(of(response));
+    fixture.ngOnInit();
+
+    expect(prodServMock.getProducts).toBeCalledTimes(1);
+    expect(fixture.products).toEqual(response);
+    expect(fixture.allCategoryProducts).toEqual(fixture.products);
+  });
+
+  it('should get the different categories', () => {
+    expect(fixture.categoryNames).toBeFalsy();
+
+    fixture.products = [{}, {}, {}];
+    fixture.getCategories();
+
+    expect(fixture.categoryNames).toBeDefined();
+  });
+
+  it('should update category on user click event', () => {
+    fixture.products = [];
+    const mockEvent = {
+      target: {
+        classList: { remove: jest.fn(), add: jest.fn() },
+        outerText: 'hello',
+      },
+    };
+
+    fixture.allCategoryProducts = [];
+
+    fixture.updateProductsCategory(mockEvent);
+
+    expect(mockEvent.target.classList.add).toBeCalledTimes(1); //Ensure a class name is added to our clicked element
+    expect(fixture.products).toBeTruthy(); //Ensure our products array contain a value.
   });
 });
