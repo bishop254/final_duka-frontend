635a892b9e5ee1d01fab423a330aecd2753575ae
bishop254 <karanjachiuri@gmail.com>
2022-05-31 13:14:31 +0300

    Displaying a single product.

---
+++ frontend/src/app/components/product/product.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faHeart, faHeartbeat, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+import { CartService } from '../../services/cart.service';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent implements OnInit {
+  id!: number;
+  product: any;
+
+  //icons
+  faBasket = faShoppingCart;
+  faHeart = faHeartbeat
+
+  constructor(
+    private route: ActivatedRoute,
+    private prodServ: ProductsService,
+    private cartServ: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((param) => {
+      this.id = param['id'];
+    });
+
+    this.prodServ.getProducts().subscribe((resp) => {
+      let allProducts = resp;
+
+      this.product = allProducts.filter(
+        (specificProduct: any) => specificProduct.id == this.id
+      );
+    });
+  }
+
+  addToCart() {
+    this.cartServ.updateCartList(this.product);
+  }
+}
