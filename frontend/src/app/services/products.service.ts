e8c71e3c22f4f00313b6ca5e6b39472567e64fc7
bishop254 <karanjachiuri@gmail.com>
2022-05-31 09:20:57 +0300

    Gave the return value of our function to be an Observable.

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +9,11 @@ export class ProductsService {
   constructor(private http: HttpClient) {}
 
   //Function to fetch products from the API
-  getProducts() {
+  getProducts(): Observable<any> {
     let url = 'https://fakestoreapi.com/products';
 
     return this.http.get(url).pipe(
-      map((resp: any) => {
-        console.log(typeof resp, 'type of API reponse');
-
+      map((resp: object) => {
         return resp;
       })
     );
