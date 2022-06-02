4794e3f7cbd4d8cd12c07d0499f8209a886f24ad
bishop254 <karanjachiuri@gmail.com>
2022-06-02 12:26:43 +0300

    Added the path to navigate to the Checkout Component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { ProductComponent } from './components/product/product.component';
@@ -9,6 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'product/:id', component: ProductComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+
   { path: '**', component: HomeComponent },
 ];
 
