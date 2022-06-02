454fa28e02b2f1f6510f9643be27135cc03f6a41
bishop254 <karanjachiuri@gmail.com>
2022-06-02 12:58:54 +0300

    Wrote test to ensure the Checkout component was working as expected.

--- frontend/src/app/components/checkout/checkout.component.spec.ts
+++ frontend/src/app/components/checkout/checkout.component.spec.ts
@@ -1,25 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup } from '@angular/forms';
 
 import { CheckoutComponent } from './checkout.component';
 
 describe('CheckoutComponent', () => {
-  let component: CheckoutComponent;
-  let fixture: ComponentFixture<CheckoutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CheckoutComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: CheckoutComponent;
+  let prodServMock: any;
+  let cartServMock: any;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CheckoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    cartServMock = {
+      cartList$: {
+        subscribe() {},
+      },
+    };
+
+    fixture = new CheckoutComponent(prodServMock, cartServMock);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture).toBeTruthy();
+  });
+
+  it('should run NgOnInit() without errors', () => {
+    expect(
+      fixture.total && fixture.cartItemsTemp && fixture.checkoutForm
+    ).toBeFalsy();
+
+    fixture.cartItemsTemp = [{ price: 1 }, { price: 1 }, { price: 1 }];
+
+    fixture.ngOnInit();
+
+    expect(fixture.checkoutForm).toBeTruthy();
+    expect(fixture.total).toBeTruthy();
+    expect(fixture.total).toEqual(parseInt((3 * 116.2).toFixed(0)));
   });
 });
