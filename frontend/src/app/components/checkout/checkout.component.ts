becaa92e74afdfb7dc22997c0e08ac9986329ca2
bishop254 <karanjachiuri@gmail.com>
2022-06-02 12:25:18 +0300

    Added the Checkout component. The logic, HTML and styling.

---
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss'],
+})
+export class CheckoutComponent implements OnInit {
+  checkoutForm!: FormGroup;
+  cartItemsTemp!: Array<any>;
+  paid: boolean = false;
+  total!: number;
+
+  constructor(
+    private prodServ: ProductsService,
+    private cartServ: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.checkoutForm = this.createFormGroup();
+
+    this.cartServ.cartList$.subscribe((resp) => {
+      this.cartItemsTemp = resp;
+    });
+
+    this.total = 0;
+    this.cartItemsTemp.forEach((product: any) => {
+      this.total += product.price;
+    });
+    this.total = parseInt((this.total * 116.2).toFixed(0));
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      mobile: new FormControl('', [
+        Validators.required,
+        Validators.minLength(13),
+      ]),
+    });
+  }
+
+  checkout() {
+    console.log('mbesha');
+  }
+}
