f53b3d5a222c4ba823a95fddb50ba3efc9adc765
bishop254 <karanjachiuri@gmail.com>
2022-06-02 16:39:55 +0300

    Added the NotificationsService to allow popup messages to appear during certain events.

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { NotificationsService } from './notifications.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +12,24 @@ export class CartService {
   private cartListSource = new BehaviorSubject<Array<any>>([]);
   cartList$ = this.cartListSource.asObservable();
 
-  constructor() {}
+  constructor(private notif: NotificationsService) {}
 
   updateCartList(product: any) {
     this.cartItems.push(...product); //Add our product to our array.
     this.cartListSource.next(this.cartItems); //Update our cart state to be the cartItems array.
+    this.notif.showSuccess('Item added to Cart');
   }
 
   removeCartItem(product: any) {
     let idx = this.cartItems.indexOf(product);
     this.cartItems.splice(idx, 1);
     this.cartListSource.next(this.cartItems);
+    this.notif.showSuccess('Item removed from Cart');
   }
 
   emptyCart() {
     this.cartItems = [];
     this.cartListSource.next(this.cartItems);
+    this.notif.showSuccess('Cart is now empty');
   }
 }
